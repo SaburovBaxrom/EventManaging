6da33985f66d9e7d875cb22802319f173ee56837
Saburov Baxrombek <104865446+SaburovBaxrom@users.noreply.github.com>
2022-12-12 14:02:15 +0500

    Update Program.cs
    change

--- exam_modul_4/Program.cs
+++ exam_modul_4/Program.cs
@@ -1,8 +1,3 @@
-﻿//using exam_modul_4.Broker;
-//using exam_modul_4.Model;
-//var broker = new DatabaseBroker();
-//User user = new User(broker.GetLastUserIdAsync().Result, "fullname","username","1234",0);
-//broker.InsertUserAsync(user).Wait();
-//Console.WriteLine();
+
 
 new exam_modul_4.UI.UI();
